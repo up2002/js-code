688a7352f0f37b2111c84a5a3348e047167a57a0
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2023-12-24 05:26:56 +0000

    complete

--- javaScript/02_datatypes.js
+++ javaScript/02_datatypes.js
@@ -1,13 +1,20 @@
 let score = "22hi"
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log((valueInNumber))
+// console.log(typeof valueInNumber)
+// console.log((valueInNumber))
 /*
 33 => number;
 "33"=> NaN
 true => 1;
 undifined => NaN
 */
+// ************************* operations **********************//
+ 
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log("1" + 2 + 2)
+// console.log(1+2+"2");
