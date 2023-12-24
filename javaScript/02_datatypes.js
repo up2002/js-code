88d40d57fe1b9745d9448bacfc364d60178731e1
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2023-12-24 05:02:32 +0000

    complete

---
+++ javaScript/02_datatypes.js
@@ -0,0 +1,13 @@
+let score = "22hi"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log((valueInNumber))
+/*
+33 => number;
+"33"=> NaN
+true => 1;
+undifined => NaN
+*/
