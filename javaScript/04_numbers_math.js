44572a3fa6eb9d29125decb5116f0d35094631a3
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2023-12-25 10:27:09 +0000

    maths

---
+++ javaScript/04_numbers_math.js
@@ -0,0 +1,23 @@
+
+//numbers
+/*
+const score = 400
+console.log(score);
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+*/
+//maths
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.3)); 
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(3,4,1,5,6));
+// console.log(Math.max(3,2,5,1,6));
+// console.log(Math.random());
+// console.log(Math.floor((Math.random())*10)+1);
+const min = 10
+const max = 20
+console.log(Math.floor((Math.random())*(max-min+1))+min);
