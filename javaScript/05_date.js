67db2d2bac812f85a57ba1c266704cf0cbcc6cb2
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2023-12-26 15:50:16 +0000

    dates

---
+++ javaScript/05_date.js
@@ -0,0 +1,20 @@
+// dates 
+
+let myDates = new Date()
+// console.log(myDates.toString());
+// console.log(myDates.toDateString());
+// console.log(myDates.toLocaleString());
+// console.log(typeof myDates);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,2)
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// when you want to second for time 
+//console.log(Math.floor(Date.now()/1000));
+let NewDate = new Date()
+console.log(NewDate);
+console.log(NewDate.getDate());
