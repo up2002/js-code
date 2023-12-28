b03cab7f097a160736b8c4591e1704f57839557c
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2023-12-28 07:16:25 +0000

    invoked method

---
+++ 03_basics/04_invoked.js
@@ -0,0 +1,10 @@
+// immediately invoked  function expressions
+// global scope ke polution se jo problem hoti hai jo bhi vaha variables wegra
+// se problem hoti hai jo vaha decleare hota hai usase dikkat na ho uske liye invoked wala method use karte hai
+(function chai() {
+    //named iife bolte hai isse
+console.log(`DB CONNECTED`);
+})();
+( (name)=> {
+    console.log(`DB CONNECTED ${name}`);
+ })("uppu");
