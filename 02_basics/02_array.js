b59971d1fca3484cec48c5c3496c17c858dd6095
Upendra Paswan <91420532+up2002@users.noreply.github.com>
2024-01-08 10:18:42 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,7 +1,7 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+//marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
@@ -13,12 +13,12 @@ const dc_heros = ["superman", "flash", "batman"]
 // isko use karne se ye fayda hota hai app chahe jitne bhi array ko concat kar sakte ho
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+ //console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+//console.log(real_another_array);
 
 
 
@@ -30,4 +30,4 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+//console.log(Array.of(score1, score2, score3));
